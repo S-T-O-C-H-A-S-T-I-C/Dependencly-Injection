102b1f87e7c47d8c87e8e5f32fd2f5e2e40cb466
Somanshu Singla <ssomanshus@gmail.com>
2019-01-22 12:34:20 +0530

    Initial Commit

---
+++ src/main/java/com/somanshu/dependencyinjection/depinject/DepinjectApplication.java
@@ -0,0 +1,15 @@
+package com.somanshu.dependencyinjection.depinject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepinjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepinjectApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/somanshu/dependencyinjection/depinject/DepinjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.somanshu.dependencyinjection.depinject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepinjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
